15a7839680e1450276d6f771c3eb5d5cbb5ba07d
barjunkey.gmail.com <barjunkey@gmail.com>
2019-09-28 10:41:53 +0200

    Add project files.

---
+++ WpfApp2/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    [ComVisible(true)] // Class must be ComVisible
+    public class Demo
+    {
+        public void SayHello(string name) => MessageBox.Show($"Hello {name} !!!");
+    }
+}
